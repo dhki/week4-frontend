8396de0482c9b48adb6e8aee95c5c3133e98022c
naco0406 <young0406@kaist.ac.kr>
2024-01-25 06:51:14 +0900

    just before the last socket

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -15,7 +15,7 @@ function KakaoLogin(){
     const loginRequest = async () => {
         if(code){
             const data = {
-                redirect_uri: 'https://week4-frontend-indol.vercel.app/login',
+                redirect_uri: 'https://spirit-xi.vercel.app/login',
                 code: code
             };
 
